40299a97d6fc552058bd105b088102be48d45928
tornadoblot <Newclearplant@gmail.com>
2016-02-18 10:01:03 +0900

    0218 update

--- 0218/0218.js
+++ 0218/0218.js
@@ -8,3 +8,20 @@
 //}).listen(300);
 //console.log('서버안녕');
 ////아침 연습으로 서버열기
+
+var http= require('http');
+
+http.createServer(function(req, res){
+    var date = new Date();
+    date.setDate(date.getDate()+7);
+    res.writeHead(200,{
+        'Content-Type': 'text/html',
+        'Set-Cookie':[
+            'breakfaste = toast; Expires = '+ date.toUTCString(),
+            'dinner = chicken'
+        ]
+    });
+    res.end('<h1>' + req.headers.cookie + '</h1>');
+}).listen(3000,function(){
+    console.log('serVer RUnasdfa');
+});
