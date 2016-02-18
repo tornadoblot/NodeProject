e9441a9860640a964e39aca42bc74cdbdce48b7d
tornadoblot <Newclearplant@gmail.com>
2016-02-18 09:45:30 +0900

    new Class Start

---
+++ 0218/0218.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Sunrin on 2016-02-18.
+ */
+//var http = require('http');
+//http.createServer(function(req, res){
+//    res.writeHead(200, {'Content-Type': 'text/plain'});
+//    res.end('Heelo,');
+//}).listen(300);
+//console.log('서버안녕');
+////아침 연습으로 서버열기
